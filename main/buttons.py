ee84a6d5647e8faf6e5e4f9632d7138874eb0a70
tarheel300 <andypguice@gmail.com>
2018-11-26 20:45:29 -0500

    Adding the first button to the GUI

---
+++ main/buttons.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, Label, Button
+
+class btn_actions():
+
+    def btn_quit_click(Tk):
+        Tk.destroy()
+
+
+
+
--- main/main.py
+++ main/main.py
@@ -1,14 +1,26 @@
-from tkinter import Tk, Label, Button
+import tkinter as tk
+#from buttons import btn_actions
 
 class my_first_gui:
-    def __init__(self, master):
-        self.master = master
-        master.title("This is my GUI")
+    def __init__(self):
+        #self.btn_actions = btn_actions()
+        self.root = tk.Tk()
+        self.root.title("This is my GUI")
 
-root = Tk()
-my_gui = my_first_gui(root)
-root.mainloop()
+        button = tk.Button(self.root, text="Quit", fg="BLUE", command=self.btn_quit_click)
+        button.pack()
 
+        self.root.mainloop()
+
+    #def btn_quit_click_window(self):
+    #    print("Hello World")
+    #    self.root.destroy()
+
+    def btn_quit_click(self):
+        self.root.destroy()
+
+
+gui = my_first_gui()
 
 
 
